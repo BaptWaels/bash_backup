00f930c411935b5594311124825cbd460f2dca60
Baptiste Waels <bwaels@gmail.com>
2015-11-24 17:30:42 +0100

    feat : Errors are well printed

--- backup.sh
+++ backup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Global variables
+ignored_file=""
+backup_dir=""
+
 #Check the shell
 if [ -z "$BASH_VERSION" ]; then
     echo -e "Error: this script requires the BASH shell!"
     exit 1
 fi
+
+function error(){
+  printf "\nERROR :\n"
+  echo " " $1
+  printf "\n"
+}
 
 # TODO Write HELP Method
 function usage(){
@@ -40,22 +50,16 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-if [ -z "$ignored_file" -a -z "$backup_dir" ]
-then
-   echo "ERROR : -i and -d options are mandatory"
+if [ -z "$ignored_file" -a -z "$backup_dir" ]; then
    usage
    exit 0
-elif [ -z "$ignored_file" ]
-then
-      echo "ERROR : -i option is mandatory"
-      usage
+elif [ -z "$ignored_file" ]; then
+      error "-i is mandatory"
       exit 0
-elif [ -z "$backup_dir" ]
-then
-          echo "ERROR : -d option is mandatory"
-          usage
+elif [ -z "$backup_dir" ]; then
+          error "-d is mandatory"
           exit 0
 fi
 
 
-# If here, it works
+# Here, we have $ignored_file and $backup_dir set
