76d68b3ea6efbc1592ed806bbb05cfd84c3f5da6
Baptiste Waels <bwaels@gmail.com>
2015-11-24 19:15:48 +0100

    feat : Process relative/absolute paths correctly

--- backup.sh
+++ backup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+
 program_name="backup.sh"
-
-
-# Global variables
 ignored_file=""
-backup_dir=""
+BACKUP_DIR=""
+CURRENT_DIR=$(pwd)"/"
 
 #Check the shell
 if [ -z "$BASH_VERSION" ]; then
@@ -12,23 +11,61 @@ if [ -z "$BASH_VERSION" ]; then
     exit 1
 fi
 
+# Format errors
 function error(){
   printf "\nERROR :\n"
   echo " " $1
   printf "\n"
 }
 
-function usage {
-    echo "usage: $program_name [-i ignored_patterns] [-d backup_dir]"
+# Print usage of the script
+function usage() {
+    echo "usage: $program_name [-i ignored_patterns] [-d BACKUP_DIR]"
     echo "	-h	                 display help"
     echo "	-i ignored_patterns	 name of the file which contains patterns to ignore"
-    echo "	-d backup_dir            Path of the directory to backup"
+    echo "	-d BACKUP_DIR            Path of the directory to backup"
     exit 1
 }
 
-###################
-#####  START  #####
-###################
+# Check if all args are set
+# Modify BACKUP_DIR variable to absolute path if needed
+function check_args(){
+  if [ -z "$ignored_file" -a -z "$BACKUP_DIR" ]; then
+     usage
+     exit 0
+  elif [ -z "$ignored_file" ]; then
+        error "-i is mandatory"
+        usage
+        exit 0
+  elif [ -z "$BACKUP_DIR" ]; then
+            error "-d is mandatory"
+            usage
+            exit 0
+  fi
+
+  ignored_file=$(echo $ignored_file | sed "s/^.\///g")   # to replace ./mypath by mypath
+  BACKUP_DIR=$(echo $BACKUP_DIR | sed "s/^.\///g")   # to replace ./mypath by mypath
+
+  if [ ! "${BACKUP_DIR:0:1}" = "/" ]; then
+    BACKUP_DIR=$CURRENT_DIR$BACKUP_DIR    # to obtain absolute path
+  fi
+
+  if [ ! "${ignored_file:0:1}" = "/" ]; then
+    ignored_file=$CURRENT_DIR$ignored_file   # to obtain absolute path
+  fi
+
+  if [ ! -d "$BACKUP_DIR" ]; then
+    error "The directory you would like to backup doesn't exist"
+  fi
+
+  if [ ! -f "$ignored_file" ]; then
+    error "The file which contains patterns to ignore doesn't exist"
+  fi
+}
+
+#################
+### DEAL ARGS ###
+#################
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables:
@@ -40,7 +77,7 @@ while getopts "hi:d:" opt; do
         ;;
     i)  ignored_file=$OPTARG
         ;;
-    d)  backup_dir=$OPTARG
+    d)  BACKUP_DIR=$OPTARG
         ;;
     *)
       usage
@@ -52,19 +89,7 @@ done
 shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
+check_args
 
-if [ -z "$ignored_file" -a -z "$backup_dir" ]; then
-   usage
-   exit 0
-elif [ -z "$ignored_file" ]; then
-      error "-i is mandatory"
-      usage
-      exit 0
-elif [ -z "$backup_dir" ]; then
-          error "-d is mandatory"
-          usage
-          exit 0
-fi
-
-
-# Here, we have $ignored_file and $backup_dir set
+echo $BACKUP_DIR
+echo $ignored_file
